72c91d62b19ad3a4d73afb48493899448ee2e160
Orlov Max <orlov.maxxim@gmail.com>
2018-03-13 13:42:57 +0300

    add action in commander, install lodash, add func of compare json files

---
+++ src/compareJsonFiles.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+export default (firstJson, secondJson) => {
+  const currentFile = JSON.parse(fs.readFileSync(firstJson));
+  const changedFile = JSON.parse(fs.readFileSync(secondJson));
+  const sharedKeys = _.keys(_.merge({}, currentFile, changedFile));
+
+  return sharedKeys.reduce((sum, current) => {
+    const currentValue = currentFile[current];
+    const changedValue = changedFile[current];
+    if (!currentValue) {
+      return sum.concat(`+ ${current} : ${changedValue}`);
+    } else if (!changedValue) {
+      return sum.concat(`- ${current} : ${currentValue}`);
+    }
+    if (currentValue === changedValue) {
+      return sum.concat(`   ${current} : ${currentValue}`);
+    }
+    return sum.concat(`+ ${current} : ${currentValue} \n - ${current} : ${changedValue}`);
+  }, []).join('\n ');
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import program from 'commander';
+import compareJsonFiles from './compareJsonFiles';
 
 const prog = () => {
   program
     .version('0.0.1')
     .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => console.log(compareJsonFiles(firstConfig, secondConfig)))
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
     .parse(process.argv);
