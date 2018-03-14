ae2b1e0f5a33d9b15f7821117c9cbec879316356
Orlov Max <orlov.maxxim@gmail.com>
2018-03-14 18:00:20 +0300

    test changed

--- __tests__/compareFiles.test.js
+++ __tests__/compareFiles.test.js
@@ -8,8 +8,8 @@ test('first test of compare JSON files', () => {
   return expect(compareJsonFiles(currentJsonFile, changedJsonFile)).toBe(correctTestAnswer);
 });
 test('second test of compare YAML files', () => {
-  const currentJsonFile = '__tests__/__fixtures__/files/firstYaml.yml';
-  const changedJsonFile = '__tests__/__fixtures__/files/secondYaml.yml';
+  const currentYamlFile = '__tests__/__fixtures__/files/firstYaml.yml';
+  const changedYamlFile = '__tests__/__fixtures__/files/secondYaml.yml';
   const correctTestAnswer = fs.readFileSync('__tests__/__fixtures__/files/testAnswer.txt').toString();
-  return expect(compareJsonFiles(currentJsonFile, changedJsonFile)).toBe(correctTestAnswer);
+  return expect(compareJsonFiles(currentYamlFile, changedYamlFile)).toBe(correctTestAnswer);
 });
