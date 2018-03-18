3495b8d32b093d031bbef6ba1fb70e39d666e1a9
Orlov Max <orlov.maxxim@gmail.com>
2018-03-18 12:48:22 +0300

    -up version-

--- src/program.js
+++ src/program.js
@@ -3,7 +3,7 @@ import compareFiles from './';
 
 export default () => {
   program
-    .version('0.1.1')
+    .version('0.1.5')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig, option) =>
       console.log(compareFiles(firstConfig, secondConfig, option.format)))
